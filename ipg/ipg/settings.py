aab1e609143919143047e3b4c6dd2fd319b1b4b3
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2023-01-15 16:09:31 +0400

    Update settings.py

--- ipg/ipg/settings.py
+++ ipg/ipg/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z=sfak966!!05bo*!iuvpe^s+lrf3j*9ieljm^q9b%j=i0#zk-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
